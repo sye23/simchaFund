04127024d69e341c22ac3542a292fdabdaf3c8a8
sye23 <shalomeisenbach@gmail.com>
2017-05-29 11:43:11 -0400

    mainly css changes

--- app/public/js/donor.js
+++ app/public/js/donor.js
@@ -5,17 +5,23 @@
   let id = $('#donorId').text();
   let donorId = Number(id);
 
-  $('#deleteDonor').click(function(){
-    console.log('clicked')
-    $.ajax({
-      url:'/donor/'+donorId,
-      type: 'DELETE',
-      success:(function(data) {
-          let url = data.replace(/['"]+/g, '');
-          console.log(url);
-          window.location.replace(url);
-        })
-      });
+  $('#deleteDonorModal').click(function(){
+    $('.deleteModal').show();
+    $('#deleteDonor').click(function(){
+      console.log('clicked');
+      $.ajax({
+        url:'/donor/'+donorId,
+        type: 'DELETE',
+        success:(function(data) {
+            let url = data.replace(/['"]+/g, '');
+            console.log(url);
+            window.location.replace(url);
+          })
+        });
+    });
+    $('#modalNoButton').click(function(){
+      $('.deleteModal').hide();
+    })
   });
 
 }());
--- app/public/js/simcha.js
+++ app/public/js/simcha.js
@@ -5,16 +5,25 @@
   let id = $('#simchaId').text();
   let simchaId = Number(id);
 
-  $('#deleteSimcha').click(function(){
-    $.ajax({
-      url:'/simcha/'+simchaId,
-      type: 'DELETE',
-      success:(function(data) {
-          let url = data.replace(/['"]+/g, '');
-          console.log(url);
-          window.location.replace(url);
-        })
-      });
+
+
+  $('#deleteSimchaModal').click(function(){
+    $('.deleteModal').show();
+    $('#deleteSimcha').click(function(){
+      $.ajax({
+        url:'/simcha/'+simchaId,
+        type: 'DELETE',
+        success:(function(data) {
+            let url = data.replace(/['"]+/g, '');
+            console.log(url);
+            window.location.replace(url);
+          })
+        });
+    });
+    $('#modalNoButton').click(function(){
+      $('.deleteModal').hide();
+    })
   });
+
 
 }());
